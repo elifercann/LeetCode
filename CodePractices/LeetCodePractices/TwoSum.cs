fcb9ed44f4c526573cfeae2dabc1e8bf4e52a548
elifercann <ercanelif450@gmail.com>
2023-08-16 17:10:56 +0300

    twosum and palindromenumber problems solved

---
+++ CodePractices/LeetCodePractices/TwoSum.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodePractices;
+
+public class Solutuion
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> numMap = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int completed = target - nums[i];
+            if (numMap.ContainsKey(completed))
+            {
+                return new int[] { numMap[completed], i };
+            }
+            numMap[nums[i]] = i;
+        }
+        throw new ArgumentException("No solution found");
+
+    }
+}
