55ac490d45a279a166dbe510fa0d9e6bcbd29a57
elifercann <ercanelif450@gmail.com>
2023-08-18 17:02:19 +0300

    RomanToInteger

---
+++ LeetCodePractices/RomanToInteger.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodePractices;
+
+public class Solut
+{
+    public int RomanToInt(string s)
+    {
+        Dictionary<char, int> romanValues = new Dictionary<char, int>
+        {
+            {'I',1 },
+            {'V',5},
+            {'X',10},
+            {'L',50 },
+            {'C',100 },
+            {'D',500 },
+            {'M',1000 }
+        };
+        int result = 0;
+        for(int i = 0; i < s.Length; i++)
+        {
+            if(i<s.Length-1&& romanValues[s[i]] < romanValues[s[i + 1]])
+            {
+                result -= romanValues[s[i]];
+            }
+            else
+            {
+                result += romanValues[s[i]];
+            }
+        }
+        return result;
+    }
+}
