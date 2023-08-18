7b1343224cdf819dc04187fe46c473b74b8ffbe8
elifercann <ercanelif450@gmail.com>
2023-08-18 17:09:58 +0300

    LongestCommonPrefix

---
+++ LeetCodePractices/LongestCommonPrefix.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractices;
+
+public class Solution4
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if(strs==null || strs.Length == 0)
+        {
+            return "";
+        }
+        string prefix = strs[0];
+        for(int i=1;i<strs.Length; i++)
+        {
+            while (strs[i].IndexOf(prefix) != 0)
+            {
+                prefix=prefix.Substring(0,prefix.Length-1);
+                if (string.IsNullOrEmpty(prefix))
+                {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+}
