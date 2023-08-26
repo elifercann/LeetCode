fda25f0d5e684ffda7e512efe7bbd6e3f630f034
elifercann <ercanelif450@gmail.com>
2023-08-26 17:08:52 +0300

    FindTheIndexOfFirstOccurrenceString solved

---
+++ LeetCodePractices/FindTheIndexOfFirstOccurrenceString.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCodePractices;
+
+public class FindTheIndexOfFirstOccurrenceString
+{
+    public int StrStr(string haystack , string needle)
+    {
+        if (string.IsNullOrWhiteSpace(needle)) 
+            return 0;
+        for (int i = 0; i <=haystack.Length -needle.Length; i++)
+        {
+            if(haystack.Substring(i, needle.Length) == needle)
+                return i;
+        }
+        return -1;
+    }
+}
