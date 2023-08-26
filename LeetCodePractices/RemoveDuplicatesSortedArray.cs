636a2a6dbe80ef0eb854a8af1f70524d0465419a
elifercann <ercanelif450@gmail.com>
2023-08-26 17:03:56 +0300

    removeelement solved

--- LeetCodePractices/RemoveDuplicatesSortedArray.cs
+++ LeetCodePractices/RemoveDuplicatesSortedArray.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetCodePractices
+﻿namespace LeetCodePractices
 {
-    internal class RemoveDuplicatesSortedArray
+    public class RemoveDuplicatesSortedArray
     {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if(nums.Length == 0) return 0;
+
+            int uniqeCount = 1;
+
+            for(int i=1; i<nums.Length; i++)
+            {
+                if (nums[i] != nums[uniqeCount-1])
+                {
+                    nums[uniqeCount] = nums[i];
+                    uniqeCount++;
+                }
+            }
+            return uniqeCount;
+        }
     }
 }
---
+++ LeetCodePractices/RemoveElement.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCodePractices
+{
+    public class RemoveElement
+    {
+        public int Remove(int[] nums,int val) 
+        {
+            int ValCount = 0;
+            for(int i=0;i<nums.Length;i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[ValCount] = nums[i];
+                    ValCount++;
+                }
+            }
+            return ValCount;
+        }
+    }
+}
