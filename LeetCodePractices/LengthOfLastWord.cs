ae3e522c575eb7d5c5da25f1df11519e5ed02e39
elifercann <ercanelif450@gmail.com>
2023-08-28 21:54:05 +0300

    Length of last word solved

---
+++ LeetCodePractices/LengthOfLastWord.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodePractices;
+
+public class LengthOfLastWord
+{
+    public int LengthLastWord(string word)
+    {
+        word = word.Trim();
+
+        int lastSpaceIndex=word.LastIndexOf(' ');
+
+        if(lastSpaceIndex == -1)
+        {
+            return word.Length;
+        }
+        else
+        {
+            return word.Length - lastSpaceIndex - 1;
+        }
+           
+    }
+}
